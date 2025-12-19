b7ba00a84fee71997fb80cfc99e783fb770bd50a
Abanda Uriel <justm6640@gmailcom>
2025-12-19 14:28:11 +0100

    chore: prepare for deployment

--- next.config.ts
+++ next.config.ts
@@ -3,6 +3,7 @@ import withPWA from "@ducanh2912/next-pwa";
 
 const config: NextConfig = {
   /* config options here */
+  output: "standalone",
   experimental: {
     serverActions: {
       allowedOrigins: ["*"],
